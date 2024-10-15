ad19a5c96ba759d71198ad5d313b2562bf01e539
Satvik Saini <88959905+SATVIK2610@users.noreply.github.com>
2024-10-15 21:02:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men" banner={men_banner} />} />
+          <Route path='/men' element={<ShopCategory category="men" banner={men_banner} />} />
           <Route path='/women' element={<ShopCategory category="women" banner={women_banner} />} />
-          <Route path='/kids' element={<ShopCategory category="kids" banner={kid_banner} />} />
+          <Route path='/kids' element={<ShopCategory category="kid" banner={kid_banner} />} />
           <Route path='product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
